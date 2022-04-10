0d29009e35648f90e24d7047b78236134379da7d
Joyce Caroline <caroline.joyce5631@gmail.com>
2022-04-10 17:21:51 -0300

    :lispstick: Finalização dos Steps com validação de Formulário

--- heyorange-app/src/components/Register/Step2.js
+++ heyorange-app/src/components/Register/Step2.js
@@ -1,11 +1,29 @@
-import { Button } from "reactstrap"
+import { Button, FormGroup } from "reactstrap"
 import React from "react"
-import RadioButton from "../RadioButton"
+import Input from "../Input"
 
 const Step2 = ({ prevStep, loading }) => {
     return (
         <div>
-            step 2
+            <h2>Conte um pouco mais sobre você</h2>
+            <p></p>
+            <p>
+                Digite até 3 Habilidades que você possuí (Exemplo: UI, Java, UX, Wireframe, AI, Angular, etc...)
+            </p>
+            <FormGroup>
+                <Input
+                    name="habilidade1"
+                    className="mb-3"
+                />
+                <Input
+                    name="habilidade2"
+                    className="mb-3"
+                />
+                <Input
+                    name="habilidade3"
+                    className="mb-3"
+                />
+            </FormGroup>
             <Button onClick={prevStep} disabled={loading}>
                 Voltar
             </Button>
