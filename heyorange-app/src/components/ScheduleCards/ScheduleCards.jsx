32a765f7dfca03c94c52754a2fa49ddb1e2621d7
Isadora <91037880+isahoelscher@users.noreply.github.com>
2022-04-13 17:56:42 -0300

    :lipstick: organizando responsividade e cards de agendamento

--- heyorange-app/src/components/ScheduleCards/ScheduleCards.jsx
+++ heyorange-app/src/components/ScheduleCards/ScheduleCards.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Card, CardBody, CardText } from "reactstrap";
+import "./ScheduleCards.css"
 
 const ScheduleCards = () => {
     return (
         <div>
-            <h4>Horários Disponíveis:</h4>
 
-            <Card className="container" color="light">
+            {/* <Card className="container" color="light"> */}
 
                 <CardBody className="body-container">
 
@@ -14,35 +14,29 @@ const ScheduleCards = () => {
                         <div className="col-4">
                             <p><strong >15/Abril<br />2022</strong></p>
                         </div>
-                        <div className="col-4">
+                        <div className="col-6">
                             <strong className="row">Anna</strong>
                             <span className="hora">🕒 15:00 - 15:30</span>
                         </div>
-                        <div className="col-4">
-                            <span>⚠️ Pendente</span>
-                        </div>
                     </CardText>
                 </CardBody>
-            </Card>
+            {/* </Card> */}
 
 
-            <Card className="container" color="light">
+            {/* <Card className="container" color="light"> */}
                 <CardBody className="body-container">
 
                     <CardText className="row">
                         <div className="col-4">
                             <p><strong >16/Abril<br />2022</strong></p>
                         </div>
-                        <div className="col-4">
-                            <strong className="row">Henrique</strong>
+                        <div className="col-6">
+                            <strong className="row">Anna</strong>
                             <span className="hora">🕒 15:00 - 15:30</span>
-                        </div>
-                        <div className="col-4">
-                            <span>⚠️ Pendente</span>
                         </div>
                     </CardText>
                 </CardBody>
-            </Card>
+            {/* </Card> */}
 
         </div>
 
