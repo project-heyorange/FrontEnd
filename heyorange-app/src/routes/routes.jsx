4ab640f4932b0034c98338887e3cab60d6b3c73f
Isadora <91037880+isahoelscher@users.noreply.github.com>
2022-04-13 16:44:22 -0300

    :sparkles: organizando redirecionamento para pagina de agendamentos

--- heyorange-app/src/routes/routes.jsx
+++ heyorange-app/src/routes/routes.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import Agendar from "../pages/Agendar";
 
 import Feed from "../pages/Feed/Feed";
 import Mentores from "../pages/Mentores";
@@ -25,6 +26,7 @@ const Router = () => {
         <Route path={"/register"} element={<Register />} />
         <Route path={"/feed"} element={<Feed />} />
         <Route path={"/mentores"} element={<Mentores/>} />
+        <Route path={"/agendar"} element={<Agendar/>} />
 
       </Routes>
     </BrowserRouter>
